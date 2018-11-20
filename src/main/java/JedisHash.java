06f407b5dda5fbb4c61a854491c9902cc4e0ec0b
xuyanMax <418780414@qq.com>
2018-11-21 01:41:17 +0800

    redis List

--- src/main/java/JedisHash.java
+++ src/main/java/JedisHash.java
@@ -6,42 +6,43 @@ import java.util.Map;
 public class JedisHash {
     private static String MYHASH = "myhash";
     private static String MYHASH2 = "myhash2";
-     public static void main(String[] args){
-         Jedis jedis = new Jedis("localhost", 6379);
 
-         jedis.hset(MYHASH, "username", "peter");
-         jedis.hset(MYHASH, "age", "18");
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost", 6379);
 
-         System.out.println(jedis.hgetAll(MYHASH));
-         System.out.println();
-         System.out.println(jedis.hkeys(MYHASH));
-         System.out.println();
-         System.out.println(jedis.hvals(MYHASH));
+        jedis.hset(MYHASH, "username", "peter");
+        jedis.hset(MYHASH, "age", "18");
 
-         jedis.hdel(MYHASH, "username");
-         System.out.println(jedis.hgetAll(MYHASH));
-         jedis.hset(MYHASH, "username", "max");
+        System.out.println(jedis.hgetAll(MYHASH));
+        System.out.println();
+        System.out.println(jedis.hkeys(MYHASH));
+        System.out.println();
+        System.out.println(jedis.hvals(MYHASH));
 
-         Map<String, String> params = new HashMap<String, String>();
-         params.put("username", "Jax");
-         params.put("age", "26");
-         params.put("sex", "boy");
-         params.put("strength", "18cm");
-         jedis.hmset(MYHASH2, params);
+        jedis.hdel(MYHASH, "username");
+        System.out.println(jedis.hgetAll(MYHASH));
+        jedis.hset(MYHASH, "username", "max");
 
-         System.out.println();
-         System.out.println(jedis.hgetAll(MYHASH2));
-         jedis.hdel(MYHASH2, "username");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("username", "Jax");
+        params.put("age", "26");
+        params.put("sex", "boy");
+        params.put("strength", "18cm");
+        jedis.hmset(MYHASH2, params);
 
-         System.out.println();
-         System.out.println(jedis.hgetAll(MYHASH2));
+        System.out.println();
+        System.out.println(jedis.hgetAll(MYHASH2));
+        jedis.hdel(MYHASH2, "username");
 
-         System.out.println(jedis.hexists(MYHASH2, "username"));
+        System.out.println();
+        System.out.println(jedis.hgetAll(MYHASH2));
 
-     }
+        System.out.println(jedis.hexists(MYHASH2, "username"));
 
-     public void sjs(String a, String... fields) {
-         for (String s:fields)
+    }
+
+    public void sjs(String a, String... fields) {
+        for (String s : fields)
             System.out.println(s);
-     }
+    }
 }
---
+++ src/main/java/JedisList.java
@@ -0,0 +1,77 @@
+import redis.clients.jedis.BinaryClient;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+
+public class JedisList {
+    private static String LIST = "mylist";
+    private static String LIST2 = "mylist2";
+    private static String LIST3 = "mylist3";
+
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost", 6379);
+
+        jedis.lpush(LIST, "1", "2", "3", "A", "1", "2", "3");
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+
+        jedis.rpush(LIST2, "1", "2", "3", "A", "1", "2", "3");
+        System.out.println(jedis.lrange(LIST2, 0, -1));
+        System.out.println();
+
+
+        jedis.lpop(LIST);
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+
+        jedis.rpop(LIST);
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+//        jedis.del(LIST);
+//        jedis.del(LIST2);
+
+        jedis.lset(LIST, 0, "A");
+        System.out.println(jedis.lrange(LIST2, 0, -1));
+
+        jedis.linsert(LIST, BinaryClient.LIST_POSITION.BEFORE, "A", "B");
+        System.out.println(jedis.lrange(LIST2, 0, -1));
+        System.out.println();
+
+        jedis.linsert(LIST, BinaryClient.LIST_POSITION.AFTER, "A", "B");
+        System.out.println(jedis.lrange(LIST2, 0, -1));
+        System.out.println();
+
+        //消息队列
+        jedis.rpoplpush(LIST, LIST2);
+        System.out.println(jedis.lrange(LIST2, 0, -1));
+        System.out.println();
+
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+
+        System.out.println(jedis.llen(LIST));
+        System.out.println();
+        System.out.println(jedis.llen(LIST2));
+        System.out.println();
+
+        System.out.println(jedis.lrem(LIST, 2, "3"));//从前向后删除两个3
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+
+
+        System.out.println(jedis.lrem(LIST, -2, "1"));//从后向前删除两个1
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+
+        System.out.println(jedis.lrem(LIST, 0, "2"));//删除所有2...
+        System.out.println(jedis.lrange(LIST, 0, -1));
+        System.out.println();
+
+
+        jedis.lpushx(LIST3, "1", "2", "9");
+        System.out.println(jedis.lrange(LIST, 0, -1));
+
+        jedis.close();
+
+    }
+}
